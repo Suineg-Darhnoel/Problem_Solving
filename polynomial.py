a7beaa81834b80c7da81bf6f58a467146b1a4aa1
Suineg-Darhnoel <t160d511@gunma-u.ac.jp>
2020-05-29 17:39:02 +0900

    Fix the computation & Use Horner's Rule

--- polynomial.py
+++ polynomial.py
@@ -2,14 +2,30 @@
 from collections.abc import Iterable
 
 class Polynomial:
+    """
+    Polynomial in the form of a_nX^n + a_(n-1)X^(n-1) + ... + a_0
+
+    """
     def __init__(self, coeffs=[]):
         self.__coeffs = coeffs
-        self.__degree = len(coeffs)
+        self.__degree = len(coeffs)-1
 
+    # compute polynomial using bruteforce
     def compute(self, x):
+        assert(self.degree >= 0)
         total = 0
-        for n in range(self.degree):
-            total += self.coeffs[n] * (x**n)
+        for n, coeff in enumerate(self.coeffs):
+            total += coeff * (x**(self.degree-n))
+        print("p({}) = {}".format(x, total))
+        return total
+
+    # compute polynomial using horner's rule
+    def horner(self, x):
+        assert(self.degree >= 0)
+        total = 0
+        for coeff in self.coeffs[:-1]:
+            total = x * (coeff + total)
+        total += self.coeffs[-1]
         print("p({}) = {}".format(x, total))
         return total
 
@@ -42,7 +58,7 @@ class Polynomial:
 
     @property
     def degree(self):
-        return len(self.coeffs);
+        return len(self.coeffs)-1;
 
     def __repr__(self):
         degree = self.degree
@@ -55,9 +71,12 @@ class Polynomial:
 
 
 x = Polynomial([3, 4, 5])
-z = Polynomial([3, 4, 5])
 y = Polynomial([4, 2, -1])
-y.compute(-10)
+z = Polynomial([3, 4, 5])
+
+print(y)
+y.compute(-100)
+y.horner(-100)
 print(x+y)
 print(x-y)
 print(x == y)
